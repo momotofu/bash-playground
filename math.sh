bddd55f90fe4b2bdf0e01b15946b59cd9b77ebd5
Christopher Reece <mrkita.reece@gmail.com>
2017-11-06 18:20:26 +0900

    commented math.sh

--- math.sh
+++ math.sh
@@ -5,6 +5,7 @@ expr 5 \* 2
 expr 5 / 2
 
 # Advanced math
+# pipe strings into the bench calculator program 'bc -l'
 echo "22 / 7" | bc -l
 echo "4.2 * 9.15" | bc -l
 echo "(6.5 / 0.5) + (6 * 2.2)" | bc -l
