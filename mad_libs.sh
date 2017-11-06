c68d7e2ccb6eb90761679feb4e958e0a8608ede0
Christopher Reece <mrkita.reece@gmail.com>
2017-11-06 19:22:50 +0900

    made a mad_libs program

---
+++ mad_libs.sh
@@ -0,0 +1,9 @@
+echo "Give me a verb"
+read verb
+echo "Give me a noun"
+read noun
+echo "give me an adjective"
+read adjective
+echo "give me an adverb"
+read adverb
+echo "The $adjective $noun went $adverb shopping but the $noun couldn't hardly $verb"
