2d563e0efbb56385c8c0e220bc3597d1b4bb969c
Christopher Reece <mrkita.reece@gmail.com>
2017-11-09 11:55:56 +0900

    fixed some stuffs

--- code/practice.sh
+++ code/practice.sh
@@ -2,19 +2,19 @@ if [[ $1 =~ ^[A-Z] ]]; then
   echo "How proper."
 fi
 
-if [[ $2 % 2 -eq 0 ]]; then
+if [[ $(($2 % 2)) -eq 0 ]]; then
   echo "Your number is even."
 else 
   echo "Your number is odd."
 fi
 
-if [[ $3 =~ '^[0-9]+' && $r =~ '^[0-9]+' ]]; then
+if [[ $3 =~ '^[0-9]+' && $4 =~ '^[0-9]+' ]]; then
   echo [[ $3 + $4 ]]
 else
   echo $3 $4
 fi
 
-if [[ date "+%a" =~ "Thu" ]]; then
+if [[ "$(date +%a)" =~ "Thu" ]]; then
   echo "Yay it's Thursday!"
 fi
 
