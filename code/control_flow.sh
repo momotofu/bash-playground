d62be49ceb790615d108cdc49e4b776903f71384
Christopher Reece <mrkita.reece@gmail.com>
2017-11-09 11:15:19 +0900

    expanded program

--- code/control_flow.sh
+++ code/control_flow.sh
@@ -2,6 +2,8 @@ echo "start program"
 
 if [[ $1 -eq 4 ]]; then
   echo "Thanks for entering $1"
+elif [[ $1 -gt 3 ]]; then
+  echo "$1 is a great number"
 else
   echo "You entered: $1, not what I was looking for."
 fi
