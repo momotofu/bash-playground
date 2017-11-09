32ac67b89c41b40a6168202cf5deb5d4358c1ab1
Christopher Reece <mrkita.reece@gmail.com>
2017-11-09 11:18:20 +0900

    added complexity

--- code/control_flow.sh
+++ code/control_flow.sh
@@ -1,9 +1,9 @@
 echo "start program"
 
-if [[ $1 -eq 4 ]]; then
+if [[ $1 -eq 3 ]] && [[ $1 -lt 7 ]]; then
   echo "Thanks for entering $1"
-elif [[ $1 -gt 3 ]]; then
-  echo "$1 is a great number"
+elif [[ $1 =~ 'Jimmy' ]] || [[ $1 =~ 'Roger' ]] || [[ $1 =~ 'Chris' ]]; then
+  echo "$1 is a great name" 
 else
   echo "You entered: $1, not what I was looking for."
 fi
